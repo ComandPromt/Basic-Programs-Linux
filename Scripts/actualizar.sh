135a06b8561bf8ade87f50f612b8d7560be297f5
Ramón Jesús Gómez Carmona <ramonjgomezcarmona@gmail.com>
2023-07-23 00:05:24 +0200

    Add files via upload

---
+++ Scripts/actualizar.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo su
+apt update
+apt upgrade
+exit 0
---
+++ Scripts/install_Basic_Programs.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+sudo su
+
+apt install -y firefox
+
+apt install -y geany
+
+apt install -y kolourpaint
+
+apt install -y default-jre default-jdk
+
+apt install -y openjdk-8-jdk
+
+apt install -y gimp
+
+apt install -y gdebi
+
+ apt install -y kazam
+
+update-alternatives --config java
+
+update-alternatives --config javac
+
+add-apt-repository ppa:christian-boxdoerfer/fsearch-daily
+
+apt update
+
+apt install -y fsearch
+
+add-apt-repository ppa:paul-climbing/ppa$ sudo apt-get update
+
+apt install -y winff
+
+add-apt-repository -y ppa:linuxuprising/shutter
+
+apt update
+
+apt install -y shutter 
+
+add-apt-repository ppa:notepadqq-team/notepadqq
+
+apt-get update
+
+apt-get install -y notepadqq
+
+apt install -y pitivi
+
+apt install -y flatpak
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+flatpak install flathub org.pitivi.Pitivi
+
+flatpak run org.pitivi.Pitivi//stable
+
+add-apt-repository ppa:ubuntuhandbook1/apps
+
+apt-get update
+
+apt-get install -y converseen
+
+apt install -y dolphin
+
+apt install -y code
+
+apt install -y handbrake
+
+apt install -y gtkhash
+
+apt install -y cheese
+
+apt install -y webcamoid
